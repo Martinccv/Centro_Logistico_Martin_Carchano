77e13c36fc406c05a8b89dd7ea6121f30dd1c7c3
Martin Carlos Carchano Vargas <164549587+Martinccv@users.noreply.github.com>
2024-07-26 15:05:57 -0300

    Update funciones.sql

--- Proyecto_centro_logistico/database_objects/funciones.sql
+++ Proyecto_centro_logistico/database_objects/funciones.sql
@@ -1,2 +1,23 @@
+USE CentroLogistico;
 
-USE my_database_change_me;
+-- Devuelve el estado de una máquina específica
+CREATE FUNCTION fn_ObtenerEstadoMaquina (@ID_Maquina INT)
+RETURNS VARCHAR(50)
+AS
+BEGIN
+    DECLARE @Estado VARCHAR(50);
+    SELECT @Estado = Estado FROM Maquinas WHERE ID_Maquina = @ID_Maquina;
+    RETURN @Estado;
+END;
+
+-- Calcula el total de materiales asignados a una obra específica
+CREATE FUNCTION fn_CalcularTotalMaterialesObra (@ID_Obra INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @TotalMateriales INT;
+    SELECT @TotalMateriales = SUM(Cantidad)
+    FROM Obras
+    WHERE ID_Obra = @ID_Obra;
+    RETURN @TotalMateriales;
+END;
