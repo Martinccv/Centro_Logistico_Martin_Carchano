49cb5f80c07ec7f9ed45b1c836d0ebbd006bf087
Martin Carlos Carchano Vargas <164549587+Martinccv@users.noreply.github.com>
2024-07-28 19:00:02 -0300

    Update triggers.sql

--- Proyecto_centro_logistico/database_objects/triggers.sql
+++ Proyecto_centro_logistico/database_objects/triggers.sql
@@ -1,35 +1,49 @@
 USE CentroLogistico;
 
--- Audita los movimientos realizados en la tabla Movimientos.
--- CREATE TRIGGER trg_AuditarMovimientos
--- AFTER INSERT, UPDATE, DELETE ON Movimientos
--- AS
---  -BEGIN
-    -- Código para auditar cambios en la tabla Movimientos
-    -- Ejemplo: Insertar datos en una tabla de auditoría
--- END;
-
--- Actualiza el stock de materiales en los depósitos cuando se aprueba una solicitud.
-DROP TRIGGER IF EXISTS trg_ActualizarStockMateriales;
-
-DELIMITER //
-CREATE TRIGGER trg_ActualizarStockMateriales
-AFTER UPDATE ON Solicitudes
+-- Validar los estados de solicitud
+CREATE TRIGGER validar_estado_solicitud
+BEFORE INSERT ON Autorizaciones
 FOR EACH ROW
 BEGIN
-    IF UPDATE(Estado)
-    BEGIN
-        DECLARE @ID_Solicitud INT, @ID_Material INT, @Cantidad INT, @ID_Deposito INT;
-        SELECT @ID_Solicitud = ID_Solicitud, @ID_Material = ID_Material, @Cantidad = Cantidad, @ID_Deposito = ID_Deposito
-        FROM INSERTED;
-        
-        IF (SELECT Estado FROM INSERTED) = 'Aprobada'
-        BEGIN
-            UPDATE Depositos
-            SET Cantidad = Cantidad - @Cantidad
-            WHERE ID_Deposito = @ID_Deposito AND ID_Material = @ID_Material;
-        END
-    END
-END; //
+    DECLARE estado_actual ENUM('Pendiente', 'Parcial', 'Aprobada', 'Rechazada');
+    
+    SELECT Estado INTO estado_actual FROM Solicitudes WHERE ID_Solicitud = NEW.ID_Solicitud;
+    
+    IF estado_actual != 'Pendiente' THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La solicitud no está en estado Pendiente.';
+    END IF;
+END;
+//
+DELIMITER ;
+
+-- 
+DELIMITER //
+
+CREATE TRIGGER actualizar_stock_materiales
+AFTER INSERT ON Detalle_Movimientos
+FOR EACH ROW
+BEGIN
+    DECLARE cantidad_disponible INT;
+    
+    -- Actualizar stock en el centro de origen
+    IF NEW.ID_Almacen_Origen IS NOT NULL THEN
+        SET cantidad_disponible = (SELECT Cantidad FROM Almacenes_Materiales WHERE ID_Almacen_Material = NEW.ID_Almacen_Origen);
+        UPDATE Almacenes_Materiales
+        SET Cantidad = cantidad_disponible - NEW.Cantidad
+        WHERE ID_Almacen_Material = NEW.ID_Almacen_Origen;
+    END IF;
+
+    -- Actualizar stock en el centro de destino
+    IF NEW.ID_Almacen_Destino IS NOT NULL THEN
+        SET cantidad_disponible = (SELECT Cantidad FROM Almacenes_Materiales WHERE ID_Almacen_Material = NEW.ID_Almacen_Destino);
+        UPDATE Almacenes_Materiales
+        SET Cantidad = cantidad_disponible + NEW.Cantidad
+        WHERE ID_Almacen_Material = NEW.ID_Almacen_Destino;
+    END IF;
+END;
+
+//
 
 DELIMITER ;
+
+-- 
